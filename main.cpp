c4b475047104b36e8c0f12e828e8dd076450c5f9
ナツリオン <k024g0060@g.neec.ac.jp>
2025-04-22 10:48:44 +0900

    yu

--- main.cpp
+++ main.cpp
@@ -1,55 +1,90 @@
-﻿#include<windows.h>
-#include<cstdint>
+﻿#include <Windows.h>
+#include <cstdint>
+#include <string>
+#include <format>
+
 //ウィンドウプロシージャ
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
-	WPARAM wparam, LPARAM lparam) {
-	switch (msg) {
-		//ウィンドウが破棄された
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// メッセージに応じてゲーム固有の処理を行う
+	switch (msg)
+	{
 	case WM_DESTROY:
 		// OSに対して、アプリの終了を伝える
-
 		PostQuitMessage(0);
 		return 0;
-
 	}
-
-	//標準のメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
-
-
 }
 
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+	auto sizeNeeded =
+		MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+	auto sizeNeeded =
+		WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+// Windowsアプリでのエントリーポイント(main関数)
 
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	// 出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
 
-
-
-
-
-//
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	WNDCLASS wc{};
-	//
+	// ウィンドウプロシージャ
 	wc.lpfnWndProc = WindowProc;
-	//
+	// ウィンドウクラス名
 	wc.lpszClassName = L"CG2WindowClass";
-	//
-	wc.hInstance = GetModuleHandle(nullptr);
-	//
+	// インスタンスハンドル
+	wc.hInstance = GetModuleHandleA(nullptr);
+
+	// カーソル
 	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
+	// ウィンドウクラスを登録する
 	RegisterClass(&wc);
 
-	//
+	// クライアント領域のサイズ
 	const int32_t kClientWidth = 1280;
 	const int32_t kClientHeight = 720;
 
-	//
+	// ウィンドウサイズを表す構造体にクライアント領域を入れる
 	RECT wrc = { 0,0,kClientWidth,kClientHeight };
 
-	//
+	// クライアント領域を元に実際のサイズにwrcを変更してもらう
 	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
 
-	//
+	// ウィンドウの生成
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,
 		L"CG2",
@@ -61,34 +96,28 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		nullptr,
 		nullptr,
 		wc.hInstance,
-		nullptr);
+		nullptr
+	);
 
-	//
-
+	//ウィンドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
 
-
 	MSG msg{};
-	//
-
-	while (msg.message != WM_QUIT) {
-		//
-		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+	//ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT)
+	{
+		// Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
+		} else
+		{
 
-		} else {
-			//
 		}
-
-
-
-
-
 	}
 
-
-	OutputDebugStringA("Hello,DirectX!\n");
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
 
 	return 0;
 }
